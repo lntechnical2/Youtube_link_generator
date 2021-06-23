08d744945d64fe1040237dfb234831981f2013a3
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-23 10:47:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
 import os
 
 TOKEN = os.environ.get("TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "")) 
+API_ID = int(os.environ.get("API_ID",12345)) 
 API_HASH = os.environ.get("API_HASH", "")
 
 
